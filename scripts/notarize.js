c31038755370b74744cecb39e1486d275e67208b
Shivam Sharma <shiv@flexontechnologies.com>
2025-04-23 19:30:08 -0700

    add apple notorization

---
+++ scripts/notarize.js
@@ -0,0 +1,16 @@
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') return;
+
+  const appName = context.packager.appInfo.productFilename;
+
+  await notarize({
+    appBundleId: 'com.GD-Notes',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASSWORD,
+    teamId: process.env.TEAM_ID,
+  });
+};
